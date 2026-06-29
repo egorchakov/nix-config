0007a987e2a151d68adbe6b3db6baad8112bd8f4
Evgenii Gorchakov <evgorchakov@gmail.com>
2026-06-30 00:12:07 +0200

    t480s dropbox

--- hosts/t480s/home.nix
+++ hosts/t480s/home.nix
@@ -13,4 +13,16 @@
     playerctl
     brightnessctl
   ];
+  systemd.user.services.dropbox = {
+    Unit = {
+      Description = "Dropbox service";
+    };
+    Install = {
+      WantedBy = [ "default.target" ];
+    };
+    Service = {
+      ExecStart = "${pkgs.dropbox}/bin/dropbox";
+      Restart = "on-failure";
+    };
+  };
 }
