fea6faf557953a789db6f28cbea664d41ea8222f
Evgenii Gorchakov <evgorchakov@gmail.com>
2026-07-21 15:59:58 +0200

    nu zellij ssh_auth_sock

--- modules/home/linux.nix
+++ modules/home/linux.nix
@@ -6,4 +6,16 @@
 
     packages = with pkgs; [ systemctl-tui ];
   };
+
+  programs.nushell.extraEnv = ''
+    if $nu.is-interactive and ($env.SSH_CONNECTION? != null) and ($env.SSH_AUTH_SOCK? != null) {
+      let forwarded_agent = $env.HOME | path join ".ssh" "forwarded-agent.sock"
+
+      if ($env.ZELLIJ? == null) and ($env.SSH_AUTH_SOCK != $forwarded_agent) {
+        ${pkgs.coreutils}/bin/ln -sfn $env.SSH_AUTH_SOCK $forwarded_agent
+      }
+
+      $env.SSH_AUTH_SOCK = $forwarded_agent
+    }
+  '';
 }
