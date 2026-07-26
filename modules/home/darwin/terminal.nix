1c0de875683d4fa94cd545a64dfa15c7ae930fc1
Evgenii Gorchakov <evgorchakov@gmail.com>
2026-07-26 19:34:24 +0200

    ghostty darwin path

--- modules/home/darwin/terminal.nix
+++ modules/home/darwin/terminal.nix
@@ -1,8 +1,9 @@
-{ pkgs, ... }: {
+{ config, pkgs, ... }: {
   programs.ghostty = {
     package = pkgs.ghostty-bin;
     settings = {
       command = "direct:${pkgs.lib.getExe pkgs.nushell}";
+      env = "PATH=/opt/homebrew/bin:/opt/homebrew/sbin:${config.home.profileDirectory}/bin:/run/current-system/sw/bin:/nix/var/nix/profiles/default/bin:/usr/local/bin:/usr/bin:/bin:/usr/sbin:/sbin";
       font-size = pkgs.lib.mkForce 16;
     };
   };
