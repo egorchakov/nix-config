a918674127a94ce3fb36c44927519e46d4f096be
Evgenii Gorchakov <evgorchakov@gmail.com>
2026-09-17 16:28:56 +0200

    rm unused input

--- flake.nix
+++ flake.nix
@@ -95,7 +95,6 @@
     agent-skills = {
       url = "github:Kyure-A/agent-skills-nix";
       inputs.nixpkgs.follows = "nixpkgs";
-      inputs.home-manager.follows = "home-manager";
     };
 
     copper-rs-skills = {
